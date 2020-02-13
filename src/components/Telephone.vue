75fa1b656af1978163c68ceeaec55136af365f46
shishir75 <shishir.srdr16@gmail.com>
2020-02-13 23:54:11 +0600

    Auto Format Input Fields

--- src/components/Telephone.vue
+++ src/components/Telephone.vue
@@ -1,10 +1,48 @@
 <template>
-    
+    <div class="mt-6">
+        <input type="text" :placeholder="template" v-model="number" class="w-56 text-2xl bg-gray-300 p-3 rounded-lg focus:outline-none">
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Telephone"
+        name: "Telephone",
+
+        props: [
+          'template'
+        ],
+        data: function () {
+            return {
+                number: '',
+                format: '',
+                regex: '^',
+            }
+        },
+
+        mounted() {
+            let x = 1;
+
+            this.format = this.template.replace(/X+/g, () => '$' + x++);
+
+            this.template.match(/X+/g).forEach((char) => {
+
+                //console.log(char.length);
+                //console.log(key);
+
+                this.regex += '(\\d{'+ char.length + '})?';
+
+                //console.log(this.regex);
+
+            });
+        },
+
+        watch: {
+            number() {
+                this.number = this.number.replace(/[^0-9]/g, '')
+                    .replace(new RegExp(this.regex, 'g'), this.format)
+                    .substr(0, this.template.length);
+            }
+        }
     }
 </script>
 
---
+++ src/views/Forms.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <telephone template="XXX-XXX-XXXXX"></telephone>
+    </div>
+</template>
+
+<script>
+
+    import Telephone from "../components/Telephone";
+
+    export default {
+        name: "Forms",
+        components: {
+            Telephone,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
